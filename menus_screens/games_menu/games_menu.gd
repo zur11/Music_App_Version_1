77df57f4ebc43fe55c714c4863069b1b08e1a075
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2023-03-04 08:58:21 -0500

    sacado games a archivos .tres

--- menus_screens/games_menu/games_menu.gd
+++ menus_screens/games_menu/games_menu.gd
@@ -1,6 +1,22 @@
 extends Control
 
 func _ready():
+	var games_directory = "res://games/games_tres/"
+	var dir = DirAccess.open(games_directory)
+
+	for node in get_node("%GotoGameButtons").get_children():
+		node.free()
+
+	if dir:
+		for filename in dir.get_files():
+			if filename.ends_with(".tres"):
+				var new_goto_game_button: GotoGameButton = load("res://menus_screens/games_menu/go_to_game_button/go_to_game_button.tscn").instantiate()
+				var new_game : Game = load(games_directory + filename)
+				new_goto_game_button.game = new_game
+				get_node("%GotoGameButtons").add_child(new_goto_game_button)
+	else:
+		printerr("An error occurred when trying to access folder.")
+
 	for goto_game_button in get_node("%GotoGameButtons").get_children():
 		goto_game_button.pressed.connect(_goto_game.bind(goto_game_button))
 
--- menus_screens/games_menu/go_to_game_button/go_to_game_button.gd
+++ menus_screens/games_menu/go_to_game_button/go_to_game_button.gd
@@ -1,3 +1,8 @@
 class_name GotoGameButton extends Button
 
 @export var game:Game
+
+func _ready():
+	if game.game_name:
+		self.text = game.game_name
+		self.name = game.game_name
